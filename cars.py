f73a89d2a05eb76897bdd846f25f625a9abb882d
Laurence de Bruxelles <laurence.de-bruxelles@stfc.ac.uk>
2018-03-01 17:54:50 +0000

    added test for the example in the problem statement

--- cars.py
+++ cars.py
@@ -4,6 +4,15 @@
 
 Self-driving cars!
 
+Example:
+
+>>> a_example = '3 4 2 3 2 10\n0 0 1 3 2 9\n1 2 1 0 0 9\n2 0 2 2 0 9\n'
+>>> solve(a_example)
+1 0
+2 2 1
 """
 
 import numpy as np
+
+def solve(input):
+    pass
