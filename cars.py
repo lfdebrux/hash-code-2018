c9815039a0fa04fbcf53a3745df5d7709e179f9e
Laurence de Bruxelles <laurence.de-bruxelles@stfc.ac.uk>
2018-03-01 18:26:52 +0000

    tests make the world go round

--- cars.py
+++ cars.py
@@ -1,33 +1,49 @@
 #!/bin/env python3
 
-"""Google Hash Code 2018 problem from 2018
+r"""Google Hash Code 2018 problem from 2018
 
 Self-driving cars!
 
 Example:
 
->>> a_example = '3 4 2 3 2 10\n0 0 1 3 2 9\n1 2 1 0 0 9\n2 0 2 2 0 9\n'
+>>> a_example = open('a_example.in', 'r')
 >>> solve(a_example)
 1 0
 2 2 1
+
+>>> Ride((0, 0), (1, 3), 2, 9)
+Ride((0, 0), (1, 3), 2, 9)
+
+>>> read_problem_statement(a_example)
+Problem(3, 4, 2, 3, 2, 10, [Ride((0, 0), (1, 3), 2, 9), Ride((1, 2), (1, 0), 0, 9), Ride((2, 0), (2, 2), 0, 9)])
 """
 
 import numpy as np
 
 class Problem:
 
-    def __init__(self, rows, cols, vehicles, bonus, steps, rides: List[Ride]):
+    def __init__(self, rows, cols, vehicles, bonus, steps, rides):
         self.rows = rows
         self.cols = cols
+        self.n_rides = len(rides)
         self.vehicles = vehicles
         self.bonus = bonus
         self.steps = steps
         self.rides = rides
 
+    def __repr__(self):
+        return f'Problem({self.rows}, {self.cols}, {self.vehicles}, {self.n_rides}, {self.bonus}, {self.steps}, {self.rides})'
+
 class Ride:
 
     def __init__(self, start, end, t_start, t_finish):
-        pass
+        self.start = start
+        self.end = end
+        self.t_start = t_start
+        self.t_finish = t_finish
+
+    def __repr__(self):
+        return f'Ride({self.start}, {self.end}, {self.t_start}, {self.t_finish})'
 
 def read_problem_statement(problem):
     # first line includes
@@ -37,7 +53,7 @@ def read_problem_statement(problem):
     # N number of rides (1≤N ≤10000)
     # B per-ride bonus for starting the ride on time (1 ≤ B ≤ 10000)
     # T number of steps in the simulation (1 ≤ T ≤ 109)
-    R, C, F, N, B, T = problem.readline().split()
+    R, C, F, N, B, T = map(int, problem.readline().split())
 
     # N subsequent lines
     # a the row of the start intersection (0 ≤ a < R)
@@ -49,8 +65,8 @@ def read_problem_statement(problem):
     # note that f can be equal to T – this makes the latest finish equal to the end of the simulation
     rides = []
     for n in range(N):
-        a, b, x, y, s, f = problem.readline.split()
-        rides.append(Vehicle((a, b), (x, y), s, f))
+        a, b, x, y, s, f = map(int, problem.readline().split())
+        rides.append(Ride((a, b), (x, y), s, f))
 
     return Problem(R, C, F, B, T, rides)
 
