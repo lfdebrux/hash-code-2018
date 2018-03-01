c2f4255a75f9209d07f5b77cdff6f8c21804294e
Laurence de Bruxelles <laurence.de-bruxelles@stfc.ac.uk>
2018-03-01 18:12:24 +0000

    add code to read problem statement into classes

--- cars.py
+++ cars.py
@@ -14,5 +14,45 @@ Example:
 
 import numpy as np
 
-def solve(input):
+class Problem:
+
+    def __init__(self, rows, cols, vehicles, bonus, steps, rides: List[Ride]):
+        self.rows = rows
+        self.cols = cols
+        self.vehicles = vehicles
+        self.bonus = bonus
+        self.steps = steps
+        self.rides = rides
+
+class Ride:
+
+    def __init__(self, start, end, t_start, t_finish):
+        pass
+
+def read_problem_statement(problem):
+    # first line includes
+    # R number of rows in the grid (0 <= R <= 10000)
+    # C number of columns of the grid (1 ≤ C ≤ 10000)
+    # F number of vehicles in the fleet (1 ≤ F ≤ 1000)
+    # N number of rides (1≤N ≤10000)
+    # B per-ride bonus for starting the ride on time (1 ≤ B ≤ 10000)
+    # T number of steps in the simulation (1 ≤ T ≤ 109)
+    R, C, F, N, B, T = problem.readline().split()
+
+    # N subsequent lines
+    # a the row of the start intersection (0 ≤ a < R)
+    # b the column of the start intersection (0 ≤ b < C )
+    # x the row of the finish intersection (0 ≤ x < R)
+    # y the column of the finish intersection (0 ≤ y < C )
+    # s the earliest start (0≤s<T)
+    # f the latest finish (0≤f ≤T), (f ≥s+|x−a|+|y−b|)
+    # note that f can be equal to T – this makes the latest finish equal to the end of the simulation
+    rides = []
+    for n in range(N):
+        a, b, x, y, s, f = problem.readline.split()
+        rides.append(Vehicle((a, b), (x, y), s, f))
+
+    return Problem(R, C, F, B, T, rides)
+
+def solve(problem):
     pass
